ccc4ba43612eaaa42efc629595d665a4eb8acbca
dengshihong <331254291@qq.com>
2014-12-20 16:44:30 +0800

    x

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,22 +1,27 @@
-import java.util.HashMap;
 
 public class FirstMissingPositive {
 	public int firstMissingPositive(int[] A) {
-		HashMap<Integer, Integer> has = new HashMap<>();
+		if(A.length == 0)
+			return 1;
 		for(int i = 0; i < A.length; i++){
-			if(A[i] > 0)
-				has.put(A[i], 0);
+			int target = A[i], tmp;
+			while(target > 0 && target - 1 < A.length && target != A[target - 1]){
+				tmp = A[target - 1];
+				A[target - 1] = target;
+				target = A[i] = tmp;
+			}
 		}
-		for(int i = 1; i < A.length + 2; i++){
-			if(!has.containsKey(i))
-				return i;
+		for(int i = 0; i < A.length; i++){
+			if(A[i] != i + 1)
+				return i + 1;
 		}
-		return 1;
+		return A.length + 1;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] A = {3, 4, -1, 1};
+		System.out.println(new FirstMissingPositive().firstMissingPositive(A));
 	}
 
 }
