1939d708e59e1c0446145e134b1caaff70a9fe63
dengshihong <331254291@qq.com>
2015-01-02 16:36:57 +0800

    x

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -11,7 +11,7 @@ public class RotateImage {
 				int tmp = matrix[i][j];
 				matrix[i][j] = matrix[l - j][i];
 				matrix[l - j][i] = matrix[l - i][l - j];
-				matrix[l - i][l - j] = matrix = matrix[j][l - i];
+				matrix[l - i][l - j] = matrix[j][l - i];
 				matrix[j][l - i] = tmp;
 			}
 		}
